26b011ff07df43ea33ddec22ea5b4bd9afd0a1fb
David Wu <davidjwu92@gmail.com>
2019-11-23 09:47:17 -0800

    added some initial folders

---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const { join } = require('path')
+
+const db = require('./config')
+const app = express()
+
+app.use(express.static(join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+require('./routes')(app)
+
+db.sync()
+  .then(() => app.listen(process.env.PORT || 3000))
+  .catch(e => console.log(e))
