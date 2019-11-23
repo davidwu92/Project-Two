b9b9bebef0ea72a89a7489e8b344f820502f8e3b
thejunglife <thejunglife@gmail.com>
2019-11-23 12:17:08 -0800

    changed routes name

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 //routes/index.js
 module.exports = app =>{
   // pass express app to other route files.
-  require('./lostRoutes.js')(app)
-  require('./foundRoutes.js')(app)
+  require('./lafRoutes.js')(app)
   require('./eventRoutes.js')(app)
 }
